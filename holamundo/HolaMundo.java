698f994929edb934a95259c73f79d7e3777e8311
cromerofajar <cromerofajar@nautilus7.danielcastelao.org>
2019-01-15 09:50:13 +0100

    Prueba1

---
+++ holamundo/HolaMundo.java
@@ -0,0 +1,20 @@
+package holamundo;
+/**
+ * Este documento esta cubierto por una licencia general de tipo GNU para mas información consulten la licencia.
+ * Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+ * Everyone is permitted to copy and distribute verbatim copies
+ * of this license document, but changing it is not allowed
+ */
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author cromerofajar
+ */
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Hola Mundo");
+    }
+    
+}
